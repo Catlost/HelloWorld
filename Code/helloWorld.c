9a1c42695d2cece6790b5f2f6b6e25d84bb0cfce
clobato <Clobato@uma.es>
2018-03-12 16:57:41 +0100

    2 news codes in c and cpp

---
+++ Code/helloWorld.c
@@ -0,0 +1,13 @@
+// The C Version used is a bit old
+
+#include <stdio.h>
+#include <stdlib.h>
+// Adding the necessary libraries
+
+int main(void) {
+
+	puts("Hello ,World!"); /* prints Hello,World! */
+	return EXIT_SUCCESS;
+}
+
+// CatLost was here
---
+++ Code/helloWorld2.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+// Initial Configuration
+
+int main() {
+
+	cout << "Hello, World!" << endl; // prints Hello, World!
+
+	return 0;
+}
+
+// CatLost was here
