1bbe999ba781d87a3c31286ebdf1f26c739b16d2
clobato <Clobato@uma.es>
2018-04-07 20:20:59 +0200

    Añadido hello World en Haskell

---
+++ Code/HelloWorld.hs
@@ -0,0 +1,8 @@
+-- You have to use a Haskell Compiler like winGHC
+-- And then write in the console the name of the function
+-- in this case you would call helloWorld 
+
+helloWorld = putStrLn "Hello, World!"
+
+
+-- Catlost, was here
